819cd3453d4fb64c553bb2a1c3d5fe584334af1a
那森 <nasen2582995949@163.com>
2023-05-24 19:21:44 +0800

    block implement encode

--- mini-lsm-starter/src/block.rs
+++ mini-lsm-starter/src/block.rs
@@ -4,8 +4,10 @@
 mod builder;
 mod iterator;
 
+const SIZE_OF_U16: usize = std::mem::size_of::<u16>();
+
 pub use builder::BlockBuilder;
-use bytes::Bytes;
+use bytes::{BufMut, Bytes};
 pub use iterator::BlockIterator;
 
 /// A block is the smallest unit of read and caching in LSM tree. It is a collection of sorted
@@ -17,7 +19,13 @@ pub struct Block {
 
 impl Block {
     pub fn encode(&self) -> Bytes {
-        unimplemented!()
+        let mut buf = Vec::<u8>::with_capacity(self.data.len() + self.offsets.len() * SIZE_OF_U16);
+        buf.put(self.data.as_slice());
+        for &offset in self.offsets.iter() {
+            buf.put_u16(offset);
+        }
+        buf.put_u16(self.offsets.len() as u16);
+        buf.into()
     }
 
     pub fn decode(data: &[u8]) -> Self {
